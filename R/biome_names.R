a32ef33c7bbe249730854e17749ef1214700a28f
mingkaijiang <shanyujordan512@gmail.com>
2017-04-11 21:02:00 +1000

    added script to store biome names

---
+++ R/biome_names.R
@@ -0,0 +1,31 @@
+# prepare biome name abbrev. list
+biomeN <- c("1. Trop m broadleaf",            ##1
+            "2. Trop d broadleaf",            ##2
+            "3. Trop conifer",                ##3
+            "4. Temp broadleaf & mixed",      ##4
+            "5. Temp conifer",                ##5
+            "6. Boreal forests",              ##6
+            "7. Trop grass",                  ##7
+            "8. Temp grass",                  ##8
+            "9. Flooded grass",               ##9
+            "10. Montane grass",               ##10
+            "11. Tundra",                      ##11
+            "12. Medit forest",                ##12
+            "13. Desert",                      ##13
+            "14. Mangroves")                   ##14
+
+# prepare biome name abbrev. list
+biome <- c("Trop m broadleaf",            ##1
+           "Trop d broadleaf",            ##2
+           "Trop conifer",                ##3
+           "Temp broadleaf & mixed",      ##4
+           "Temp conifer",                ##5
+           "Boreal forests",              ##6
+           "Trop grass",                  ##7
+           "Temp grass",                  ##8
+           "Flooded grass",               ##9
+           "Montane grass",               ##10
+           "Tundra",                      ##11
+           "Medit forest",                ##12
+           "Desert",                      ##13
+           "Mangroves")                   ##14
